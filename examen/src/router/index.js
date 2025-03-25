d2d6ff4e171c134e567ac2997945da3832e4e7d9
mcalex468 <alexmaco468@gmail.com>
2025-03-25 17:26:21 +0100

    commit navigation & app vue

--- examen/src/router/index.js
+++ examen/src/router/index.js
@@ -5,9 +5,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/',name: 'home', component: HomeView, },
-    { path: '/beques', name: 'beques',  component: () => import('../views/AboutView.vue'), },
-    { path: '/beques/:centres', name: 'beques',  component: () => import('../views/AboutView.vue'), },
-    { path: '/beques/:centres/:any', name: 'beques',  component: () => import('../views/AboutView.vue'), },
+    
+    { path: '/beques', name: 'beques',  
+      component: () => import('../views/AboutView.vue'), },
+
+    { path: '/beques/:centres', name: 'beques',  
+      component: () => import('../views/AboutView.vue'), },
+
+    { path: '/beques/:centres/:any', name: 'beques',  
+      component: () => import('../views/AboutView.vue'), },
   ],
 })
 
